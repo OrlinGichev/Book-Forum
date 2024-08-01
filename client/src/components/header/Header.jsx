d8490f02db49e0bad31dc1d92a8a849f2fd1a3a7
OrlinGichev <o.gichev.genik@gmail.com>
2024-08-01 22:33:13 +0300

    add Header.css

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -5,7 +5,8 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import './Header.css';
 
 
 export default function Header ()  {
