ed2645550bc3fb93b61f95954a88d7dbaac1fc14
OrlinGichev <o.gichev.genik@gmail.com>
2024-08-05 19:11:26 +0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,13 @@
 // import React from 'react';
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-dom/client';
+import App from './App.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-  <App />
+  <BrowserRouter>
+  
+      <App />
+  
+  </BrowserRouter>
 )
