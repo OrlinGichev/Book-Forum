294841a28623b701f44f66be6d53902791f5044f
OrlinGichev <o.gichev.genik@gmail.com>
2024-08-05 19:49:42 +0300

    Add Home component and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 // import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import './styles.css';
 
 import Header from "./components/header/Header";
 import BookList from './components/book-list/BookList';
+import Login from './components/login/Login';
+import Home from './components/home/Home';
 
 
 
@@ -11,10 +14,14 @@ function App() {
 
 
   return (
-    <>
+    <div id="app">
       <Header/>
-      <BookList/>
-    </>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/books" element={<BookList />} />
+              <Route path="/login" element={<Login />} />
+          </Routes>
+    </div>
   )
 }
 
--- client/src/components/book-list/BookList.jsx
+++ client/src/components/book-list/BookList.jsx
@@ -1,44 +1,65 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 import "./BookList.css";
 
 export default function BookList() {
-
-    return (
-        <div className="card-list">
-            <Card  className="card-item">
-            <Card.Img variant="top" src="../../../public/book-item.jpg" />
-            <Card.Body >
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the  content.
-              </Card.Text>
-              <Button variant="primary">Details</Button>
-            </Card.Body>
-            </Card>
-            <Card  className="card-item">
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body >
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the  content.
-              </Card.Text>
-              <Button variant="primary">Details</Button>
-            </Card.Body>
-            </Card>
-            <Card  className="card-item">
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body >
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the  content.
-              </Card.Text>
-              <Button variant="primary">Details</Button>
-            </Card.Body>
-            </Card>
-        </div>
-    );
+  return (
+    <div className="card-list">
+      <Card className="card-item">
+        <Card.Img variant="top" src="../../../public/book-item.jpg" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the content.
+          </Card.Text>
+          <Button variant="primary">Details</Button>
+        </Card.Body>
+      </Card>
+      <Card className="card-item">
+        <Card.Img variant="top" src="../../../public/book-item.jpg" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the content.
+          </Card.Text>
+          <Button variant="primary">Details</Button>
+        </Card.Body>
+      </Card>
+      <Card className="card-item">
+        <Card.Img variant="top" src="../../../public/book-item.jpg" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the content.
+          </Card.Text>
+          <Button variant="primary">Details</Button>
+        </Card.Body>
+      </Card>
+      <Card className="card-item">
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the content.
+          </Card.Text>
+          <Button variant="primary">Details</Button>
+        </Card.Body>
+      </Card>
+      <Card className="card-item">
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the content.
+          </Card.Text>
+          <Button variant="primary">Details</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
