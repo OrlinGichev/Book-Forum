22283f002c81b457cd39236dab70658016061700
OrlinGichev <o.gichev.genik@gmail.com>
2024-08-07 22:48:04 +0300

    Upgrade request lib

--- client/src/services/bookService.js
+++ client/src/services/bookService.js
@@ -1,23 +1,15 @@
-import { request } from "../lib/request";
+import * as request from "../lib/request";
 
 const baseUrl = 'http://localhost:3030/jsonstore/books';
 
 export const getAll = async () => {
-    const result = await request('GET', baseUrl);
+    const result = await request.get(baseUrl);
 
     return Object.values(result);
 };
 
 export const create = async (data) => {
-    const response = await fetch(`${baseUrl}`, {
-        method: 'POST',
-        headers: { 
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-        });
-
-    const result = await response.json();
+       const result = await request.post(baseUrl, data);
 
     return result;
 }
