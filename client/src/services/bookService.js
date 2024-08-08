f6483831a4a147c76386b24189a5e4a3cb56c058
OrlinGichev <o.gichev.genik@gmail.com>
2024-08-08 20:53:49 +0300

    add getOne bookService

--- client/src/services/bookService.js
+++ client/src/services/bookService.js
@@ -2,6 +2,12 @@ import * as request from "../lib/request";
 
 const baseUrl = 'http://localhost:3030/jsonstore/books';
 
+export const getOne = async (bookId) => {
+    const result = await request.get(`${baseUrl}/${bookId}`);
+
+    return result;
+}
+
 export const getAll = async () => {
     const result = await request.get(baseUrl);
 
