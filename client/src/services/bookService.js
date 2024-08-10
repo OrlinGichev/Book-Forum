351af241cdbe1566da9183e65823593780c49b6d
OrlinGichev <o.gichev.genik@gmail.com>
2024-08-10 15:57:24 +0300

    migrate to collection from jsonstore

--- client/src/services/bookService.js
+++ client/src/services/bookService.js
@@ -1,6 +1,6 @@
 import * as request from "../lib/request";
 
-const baseUrl = 'http://localhost:3030/jsonstore/books';
+const baseUrl = 'http://localhost:3030/data/books';
 
 export const getOne = async (bookId) => {
     const result = await request.get(`${baseUrl}/${bookId}`);
